d97301ae169b7c285a5d20d94cc7dba9bc484600
henry-dura <henryikemefuna@gmail.com>
2022-10-10 18:05:23 +0100

    error message  update

--- javascript.js
+++ javascript.js
@@ -4,7 +4,8 @@ const confirmPwd = document.querySelector('#confirm-pwd');
 
 button.addEventListener('click',(e)=>{
     if(pwd.value !== confirmPwd.value){
-        alert('form Mismatch')
+        alert('The password do not match');
+        confirmPwd.value = '';
         e.preventDefault();
     }
 
